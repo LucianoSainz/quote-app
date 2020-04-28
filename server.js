3b032d174484e878b616fbf151f499b6104cd2f7
luciaoSainz <danielsainz28@gmail.com>
2020-04-28 11:21:19 +0200

    added refactoring

--- server.js
+++ server.js
@@ -10,21 +10,21 @@ console.log("Server listening at: 3000");
 
 //Handling the default route
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-//Handling the route  for admin
-app.get('/admin' , function(req, res) {
-    res.sendFile(__dirname + '/admin.html');
+//Handling the route for admin
+app.get('/admin' , function(req,res) {
+    res.sendFile(__dirname + '/admin.html'); 
 })
 
 //Code for sockets
-io.on('connection', function (socket){
+io.on('connection', function (socket) {
+  
+  socket.emit('welcome', { data: 'welcome' });
 
-    socket.emit('welcome', { data: 'welcome'});
-
-    socket.on('new' , function(data) {
-        console.log('About to upload Quote')
-        io.sockets.emit('next' , { data : data})
+  socket.on('new' , function(data) {   
+          console.log('About to upload Quote')
+          io.sockets.emit( 'next' , { data : data } )
     })
 });
